4b5ea3d7aea02497a292949aa1eec12c545c91f6
Katuhito Hara <katuhitohara@gmail.com>
2021-08-12 17:38:53 +0900

    First Settings

--- katuProject/settings.py
+++ katuProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'katu001',
+    'katu001.apps.Katu001Config',
 ]
 
 MIDDLEWARE = [
