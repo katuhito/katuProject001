d5a48def7a4d0e75abff0fc5391bcf58b12f2690
Katuhito Hara <katuhitohara@gmail.com>
2021-08-17 09:39:36 +0900

    model create

--- katu001/models.py
+++ katu001/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+
+    CATEGORY = (('science', '科学のこと'), ('dailytime', '日常のこと'), ('music', '音楽のこと'))
+
+    #タイトル用のフィールド
+    title = models.CharField(
+        verbose_name = 'タイトル', 
+        max_length = 200
+    )
+    #本文用のフィールド
+    content = models.TextField(
+        verbose_name = '本文',
+    )
+    #投稿日時のフィールド
+    posted_at = models.DateTimeField(
+        verbose_name = '投稿日時',
+        auto_now_add = True
+    )
+    #カテゴリのフィールド
+    category = models.CharField(
+        verbose_name = 'カテゴリ',
+        max_length = 50,
+        choices = CATEGORY
+    )
+
+    def __str__(self):
+        """オブジェクトを文字列に変換して返す
+        Returns(str):投稿記事のタイトル
+        """
+        return self.title
+
+
+
+
