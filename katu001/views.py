9d9fc4c8eb46a7da595ef7d386b9169a0ec845cf
Katuhito Hara <katuhitohara@gmail.com>
2021-08-17 14:31:32 +0900

    toppage index view

--- katu001/views.py
+++ katu001/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .models import BlogPost
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index.html'
+    #object_listキーの別名を設定
+    context_object_name = 'orderby_records'
+    #モデルBlogPostのレコードのオブジェクトにorder_by()を適用して
+    #BlogPostのレコードを投稿日時の降順で並べ替える。
+    queryset = BlogPost.objects.order_by('-posted_at')
+    
+
+
     
 
